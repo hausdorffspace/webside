59ed3a1c5b3177c5a51c014470265d4d778505c4
hausdorffspace <rafal.antas96@gmail.com>
2020-08-05 18:46:40 +0200

    remove from insert user_id because of sql exeption

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -1,21 +1,17 @@
 INSERT INTO user_role(description,role) VALUES ('default role for user','ROLE_USER');
 
-INSERT INTO article(content,relase_date,title,user_id) VALUES ('this is a content of first article',
+INSERT INTO article(content,relase_date,title) VALUES ('this is a content of first article',
                                                                 '2019-01-10 22:22:31',
-                                                                'pochodne kowariantne',
-                                                                1);
+                                                                'pochodne kowariantne');
 
-INSERT INTO article(content,relase_date,title,user_id) VALUES ('this is a content of second article',
+INSERT INTO article(content,relase_date,title) VALUES ('this is a content of second article',
                                                                 '2019-01-10 23:22:31',
-                                                                'pochodne kowariantne',
-                                                                1);
+                                                                'pochodne kowariantne');
 
-INSERT INTO article(content,relase_date,title,user_id) VALUES ('this is a content of third article',
+INSERT INTO article(content,relase_date,title) VALUES ('this is a content of third article',
                                                                 '2019-01-10 12:22:00',
-                                                                'pochodne kowariantne',
-                                                                2);
+                                                                'pochodne kowariantne');
 
-INSERT INTO article(content,relase_date,title,user_id) VALUES ('Emilka jest super',
+INSERT INTO article(content,relase_date,title) VALUES ('Emilka jest super',
                                                                 '2019-01-10 09:42:31',
-                                                                'pochodne kowariantne',
-                                                                99);
+                                                                'pochodne kowariantne');
